cacb5ce148e9b48336a6c3e88d0df5381b9ff6e2
Muthu4558 <muthukdm45@gmail.com>
2025-10-06 16:50:46 +0530

    bug fixed in live

--- server/server.js
+++ server/server.js
@@ -9,7 +9,21 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // allow Postman or server-to-server requests
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
